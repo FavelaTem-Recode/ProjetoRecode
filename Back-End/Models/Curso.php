aad22f8143540b735c360eb9542b5b0e43ce48b3
rafaelveja <rafaelveja@gmail.com>
2021-01-03 00:54:12 -0300

    banco de dados e php rafael

---
+++ Back-End/Models/Curso.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'Connection.php';
+
+class Cursos {
+
+    public $idcursos;
+    public $nome_curso;
+    public $pontuacao;
+    public $descricao;
+    public $imagem;
+
+    public static function selectAllCursos()
+    {
+        $conn = Connection::getConnection();
+        $stmt = $conn->query("SELECT * FROM cursos;");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+    public function selectCursos()
+    {
+        $conn = Connection::getConnection();
+        $stmt = $conn->query("SELECT * FROM cursos left outer join perguntas on idcursos = fk_curso where idcursos = '$this->idcursos';");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+}
+
+
