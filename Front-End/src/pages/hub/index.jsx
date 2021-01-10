b2be92e16490a192fe6d50e907684f74e847170c
andre-felipe1991 <andre_felipe.amorim@hotmail.com>
2021-01-10 04:14:23 -0300

    termino da page hub

--- Front-End/src/pages/hub/index.jsx
+++ Front-End/src/pages/hub/index.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './stylehub.css';
 
-import logo from '../../assets/imagens/logofavelatem.png'
+import logo from '../../assets/imagens/logofavelatem.png';
+import consultoria from "../../assets/imagens/consultoria.png";
+import marketing from "../../assets/imagens/marketing-digital.png";
+import empreendedorismo from "../../assets/imagens/empreendedorismo.png";
 
 const Hub = () => {
-    return(
+    return (
         <div className="container-fluid">
             <div className="section-1">
                 <div className="bloco-1">
@@ -18,39 +21,87 @@ const Hub = () => {
                     </div>
                 </div>
             </div>
-            <div className="section-user">
-                <aside className="inf-user">
-                    <div className="foto-user">
-                        <img src="" alt="foto usuario"/>
+            <div className="section-principal">
+                <div className="section-user">
+                    <aside className="inf-user">
+                        <div className="foto-user">
+                            <img src="" alt="foto usuario" />
+                        </div>
+                        <div className="name-user">
+                            <p>Nome Usuario/Loja</p>
+                        </div>
+                        <div className="pontos">
+                            <p>50</p>
+                            <p>pontos</p>
+                        </div>
+                        <div className="atividades">
+                            <p>1/3</p>
+                            <p>atividades</p>
+                        </div>
+                        <div className="anuncios">
+                            <p>10</p>
+                            <p>anúncios</p>
+                        </div>
+                        <div className="experiencia">
+                            <ul>
+                                <li>Nivel</li>
+                                <li>EXP</li>
+                            </ul>
+                        </div>
+                        <div className="btns">
+                            <button className="btn">Portifólio</button>
+                            <button className="btn">Perfil</button>
+                        </div>
+                    </aside>
+                </div>
+                <div className="conteudo">
+                    <div className="cursos">
+                        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
+                                <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
+                                <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src={consultoria} class="consultoria-empresarial" alt="..." />
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>Consultoria</h5>
+                                        <p>Um curso completo com dicas para melhor administrar seu negócio.</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src={marketing}class="d-block w-100" alt="..." />
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>Marketing Digital</h5>
+                                        <p>Descubra como seu negócio pode evoluir com nosso curso de Marketing.</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src={empreendedorismo} class="d-block w-100" alt="..." />
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>Empreendedorismo</h5>
+                                        <p>Otimas dicas e sugestões para seu negócio evoluir!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Anterior</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Próximo</span>
+                            </a>
+                        </div>
                     </div>
-                    <div className="name-user">
-                        <p>Nome Usuario/Loja</p>
-                    </div>          
-                    <div className="pontos">
-                        <p>50</p>
-                        <p>pontos</p>
+
+                    <div className="propaganda">
+                        <h1>Sua Empresa aqui! Sendo vista por mais de 400 Mil Pessoas.</h1>
                     </div>
-                    <div className="atividades">
-                        <p>1/3</p>
-                        <p>atividades</p>
-                    </div>
-                    <div className="anuncios">
-                        <p>10</p>
-                        <p>anúncios</p>
-                    </div>
-                    <div className="experiencia">
-                        <ul>
-                            <li>Nivel</li>
-                            <li>EXP</li>
-                        </ul>                        
-                    </div>
-                    <div>
-                        <button>Portifólio</button>
-                        <button>Perfil</button>
-                    </div>
-                </aside>
+                </div>
             </div>
-        </div>        
+        </div>
     )
 }
 
