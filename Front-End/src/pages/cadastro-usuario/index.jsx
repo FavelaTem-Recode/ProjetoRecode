4dbba732ad950e08f68dcb65b5b805c33036f117
andre-felipe1991 <andre_felipe.amorim@hotmail.com>
2021-01-14 23:00:57 -0300

    alteração no formulario de cadastro de usuario

--- Front-End/src/pages/cadastro-usuario/index.jsx
+++ Front-End/src/pages/cadastro-usuario/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './stylerg.css';
+import './style.css';
 
 import Voltar from '../../assets/imagens/icons8-voltar-26.png';
 import Alerta from '../../assets/imagens/alert.png';
@@ -7,74 +7,72 @@ import Alerta from '../../assets/imagens/alert.png';
 const Registro = () => {
     return (
 
-        <div className="registro">
-
-            <div class="jumbotron jumbotron-fluid">
-                <div class="image">
-                    <img src={Voltar} alt="voltar" />
-                </div>
-                <h3 class="title">Cadastre-se no Favela Tem e encontre um profissional Paraísopolis.</h3>
-
-            </div>
+        <div className='cadastro'>
+            <header className='jumbotron jumbotron-fluid pt-4 h-25 section-1'>             
+                    <button><img src={Voltar} alt="botão para voltar" /></button>
+                    <p>Cadastre-se no Favela Tem e encontre um profissional para sua necessidade.</p>
+                    <hr />
+                
+            </header>
 
 
-            <div class="row d-flex justify-content-center">
+            <main className='row d-flex justify-content-center'>
+                
+                    <div className="h3 col-sm-6 form bg-white">
+                        <form action="post">
+                            <div className='formulario'>
+                                <p>Insira seu dados abaixo</p>
 
-                <div class="h3 col-6 form bg-white">
+                                <div class="form-group">
+                                    <label for="nome">Nome completo:</label>
+                                    <input type="text" className="form-control" id="nome" placeholder="Digite seu nome completo"
+                                        name="nome" required />
+                                </div>
 
-                    <form>
-                        <div class="box.int">
-                            <h3>Insira seus dados abaixo:</h3>
-                            <hr />
+                                <div class="form-group">
+                                    <label for="email">Email:</label>
+                                    <input type="email" className="form-control" name="email" id="email"
+                                        placeholder="Digite seu email" />
+                                </div>
 
-                            <div class="form-group">
-                                <label for="nome">Nome completo:</label>
-                                <input type="text" class="form-control" id="nome" placeholder="Digite o nome do seu serviço" name="nome" required />
+                                <div class="form-group">
+                                    <label for="password">Senha:</label>
+                                    <input type="password" className="form-control" name='senha' id='senha'
+                                        placeholder="Crie sua senha" />
+                                </div>
 
+                                <div class="form-group">
+                                    <label for='confpassword'>Confirme sua senha:</label>
+                                    <input type="password" className='form-control' name="confsenha" id="confsenha" placeholder="Confirme sua senha" />
+                                </div>
+
+
+                                <div class="form-group">
+                                    <input type="checkbox" className="checkbox" name="checkbox" id="checknox" />
+                                    <label for="checkbox" className="form-check-label"></label>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="email">Endereço de email:</label>
-                                <input type="email" class="form-control" id="email" placeholder="Digite aqui seu email" />
-
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Senha:</label>
-                                <input type="password" class="form-control" id="password" placeholder="Crie uma senha" />
-                            </div>
-                            <div class="form-group">
-                                <label for="confpassword">Confirme sua senha:</label>
-                                <input type="password" class="form-control" id="confpassword" placeholder="Confirme sua senha" />
-                            </div>
-
-                            <div class="form-group form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                                <label class="form-check-label" for="exampleCheck1">Li e concordo com os termos de uso e a Política de Privacidade.</label>
-                            </div>
-                        </div>
-
-                    </form>
-                    <div class="footer">
-                        <footer>
-                            <div class="alert">
-
-                                <img src={Alerta} alt="alert" />
-                                <p>Importante!<br />Preencha todos os dados.</p>
-                            </div>
-
-                            <div>
-                                <button type="submit" class="btn">Salvar cadastro</button>
-                            </div>
-                        </footer>
+                            <footer className="footer">
+                                <div className='alerta'>
+                                    <img src={Alerta} alt='atenção' />
+                                    <p>Atenção <br /> Preencha todos os dados!</p>
+                                </div>
+                                <div className="cadastrar">
+                                    <input type="button" value="Cadastrar" />
+                                </div>
+                            </footer>
+                        </form>
                     </div>
-                </div>
+                
 
-            </div>
-
+            </main>
         </div>
 
 
     )
 
-
 }
 export default Registro;
+
+
+
--- Front-End/src/pages/login/index.jsx
+++ Front-End/src/pages/login/index.jsx
@@ -13,36 +13,38 @@ const Login = () => {
                 </div>
             </div>
 
-            <div class="container-fluid flex-column d-flex h-75 align-items-center justify-content-top paraisopolis">
-                <div class="form bg-white w-75 px-3 pt-3">
-                    <h3>Seja bem-vindo(a)!</h3>
-                    <div class="link">
-                        <a href="#">
-                            Ainda não tem uma conta?
+            <div class="row container-fluid flex-column d-flex h-75 align-items-center justify-content-top paraisopolis">
+                <div className='col-sm-8'>
+                    <div class="form bg-white w-100 px-3 pt-3">
+                        <h3>Seja bem-vindo(a)!</h3>
+                        <div class="link">
+                            <a href="#">
+                                Ainda não tem uma conta?
                         </a>
+                        </div>
+                        <hr />
+                        <form className="form-log">
+                            <div class="form-group">
+                                <label for="email">Endereço de email</label>
+                                <input type="email" class="form-control" id="email" placeholder="Digite aqui seu email" />
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Senha</label>
+                                <input type="password" class="form-control" id="password" placeholder="Digite sua senha" />
+                            </div>
+                            <div class="form-group form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                <label class="form-check-label" for="exampleCheck1">Lembrar sua senha</label>
+                            </div>
+                        </form>
+                        <div class="link">
+                            <a href="#">Esqueceu sua senha?</a>
+                        </div>
                     </div>
-                    <hr />
-                    <form className="form-log">
-                        <div class="form-group">
-                            <label for="email">Endereço de email</label>
-                            <input type="email" class="form-control" id="email" placeholder="Digite aqui seu email" />
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Senha</label>
-                            <input type="password" class="form-control" id="password" placeholder="Digite sua senha" />
-                        </div>
-                        <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                            <label class="form-check-label" for="exampleCheck1">Lembrar sua senha</label>
-                        </div>
-                    </form>
-                    <div class="link">
-                        <a href="#">Esqueceu sua senha?</a>
-                    </div>
-                </div>
-                <footer className="w-75 p-3">
+                    <footer className="w-100 p-3">
                         <button type="submit" class="btn m-0">Entrar</button>
-                </footer>
+                    </footer>
+                </div>
 
             </div>
         </div>
