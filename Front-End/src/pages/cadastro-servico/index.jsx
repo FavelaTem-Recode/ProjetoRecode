9da493dd4069c66ec9028bbba109a006e5b5de05
carolinebeirao <carolinebeiraoo@gmail.com>
2021-01-17 00:56:29 -0300

    atualização pagina de cadastro de serviço

--- Front-End/src/pages/cadastro-servico/index.jsx
+++ Front-End/src/pages/cadastro-servico/index.jsx
@@ -6,122 +6,79 @@ import Alerta from '../../assets/imagens/alert.png';
 
 const Servico = () => {
     return (
-
         <div className="servico">
-
-            <div class="jumbotron jumbotron-fluid">
+            <div class="jumbotron jumbotron-fluid h-25 pt-4 header-servico">
                 <div class="image">
-                    <img src={Voltar} alt="voltar" />
+                    <button><img src={Voltar} alt="voltar" /></button>
                 </div>
-                <h3 class="title">Seja um profissional disponível no Favela Tem!</h3>
-
             </div>
-
-            <div class="row d-flex justify-content-center">
-
-                <div class="h3 col-6 form bg-white">
-
-                    <div class="box.int">
-                        <h3>Cadastre-se aqui e seja visto:</h3>
-                        <hr />
-
+            <div class="container-fluid flex-column d-flex h-75 align-items-center justify-content-top paraisopolis">
+                <div class="form bg-white w-50 px-4 pt-3">
+                    <h3>Dados do serviço!</h3>
+                    <hr />
+                    <form className="form-log">
                         <div class="form-group">
                             <label for="nome">Nome do serviço:</label>
                             <input type="text" class="form-control" id="nome" placeholder="Digite o nome do seu serviço"
                                 name="nome" required />
-
                         </div>
                         <div class="form-group">
                             <label for="nome">Categorias (separe com vírgula):</label>
                             <input type="text" class="form-control" id="nome"
                                 placeholder="Digite a categoria, ex: moda, beleza, manutenção" name="nome" required />
-
                         </div>
                         <div class="form-group">
                             <label for="nome">Descreva o seu serviço aqui:</label>
                             <input type="text" class="form-control" id="nome" placeholder="Digite aqui o que você faz"
                                 name="nome" required />
-
                         </div>
-                        <div class="checkbox1">
-
-                            <div class="p custom-control custom-radio">
-                                <p>Atendimento:</p>
-                                <input class="custom-control-input" type="checkbox" name="caracteristica" id="alto"
-                                    value="Alto" />
-                                <label class="custom-control-label" for="alto">
-                                    Em domicílio
-                        </label>
+                        <div class="checks">
+                            <div class="atendimento custom-control">
+                                <p>Atendimento</p>
+                                <input class="custom-control-input" type="checkbox" name="caracteristica" id="alto" value="Alto" />
+                                <label id="pclaro" class="custom-control-label" for="alto">
+                                    Domicilio</label>
+                                <input class="custom-control-input" type="radio" name="caracteristica" id="rapido" value="Rápido" />
+                                <label id="pclaro" class="custom-control-label" for="rapido">
+                                    No local </label>
                             </div>
-                            <div class="p custom-control custom-radio">
-                                <input class="custom-control-input" type="radio" name="caracteristica" id="rapido"
-                                    value="Rápido" />
-                                <label class="custom-control-label" for="rapido">
-                                    No local
-                        </label>
+                            <div class="atendimento custom-control">
+                                <p >Pagamento</p>
+                                <input class="custom-control-input" type="checkbox" name="caracteristica" id="alto" value="Alto" />
+                                <label id="pclaro" class="custom-control-label" for="alto">
+                                    Dinheiro</label>
+                                <input class="custom-control-input" type="radio" name="caracteristica" id="rapido" value="Rápido" />
+                                <label id="pclaro" class="custom-control-label" for="rapido">
+                                    Cartão </label>
                             </div>
-                            <br />
-                            <div class="p custom-control custom-radio">
+                            <div class="p custom-control">
                                 <p>Horário de atendimento:</p>
-                                <input class="custom-control-input" type="checkbox" name="caracteristica" id="alto"
-                                    value="Alto" />
-                                <label class="custom-control-label" for="alto">
-                                    Das
-                        </label>
+                                <textarea type="date" class="custom-control-input" name="caracteristica" id="alto"
+                                    value="" />
+                                <textarea class="custom-control-label" for="alto">
+                                </textarea>
                             </div>
-                            <div class="p custom-control custom-radio">
-                                <input class="custom-control-input" type="checkbox" name="caracteristica" id="rapido"
-                                    value="Rápido" />
-                                <label class="custom-control-label" for="rapido">
-                                    Até
-                        </label>
-                            </div>
-                        </div>
-                    </div>
-                    <br />
-                    <div class="checkbox2">
-                        <div class="custom-control custom-radio">
-                            <p>Pagamento:</p>
-                            <input class="custom-control-input" type="checkbox" name="caracteristica" id="alto" value="Alto" />
-                            <label class="custom-control-label" for="alto">
-                                Apenas dinheiro
-                    </label>
-                        </div>
-                        <div class="custom-control custom-radio">
-                            <input class="custom-control-input" type="checkbox" name="caracteristica" id="rapido"
-                                value="Rápido" />
-                            <label class="custom-control-label" for="rapido">
-                                Aceitamos cartão
-                    </label>
-                        </div>
-                    </div>
-                    <br />
-                    <div class="file">
-                        <p>Envie a foto do ser serviço. Capriche pois esta será a cara do seu anúncio!</p>
-                    </div>
-                    <form>
-                        <div class="file form-file">
-                            <label class-for="file">Clique aqui para anexar ou arraste o arquivo para cá.</label>
-                            <input type="file" class="form-control-file" id="file" />
                         </div>
                     </form>
-                    <div class="footer">
-                        <footer>
-                            <div class="alert">
-
-                                <img src={Alerta} alt="alert" />
-                                <p>Importante!<br />Preencha todos os dados.</p>
-                            </div>
-
-                            <div>
-                                <button type="submit" class="btn">Cadastrar</button>
-                            </div>
-                        </footer>
-                    </div>
                 </div>
+                <footer className="w-50 p-3">
+                    <div className="azul">
+                        <div className="inferior">
+                            <div id="alerta">
+                                <img src={Alerta} alt="voltar" />
+                            </div>
+                            <div id="alertap">
+                                <p>Importante!</p>
+                                <p>Preencha todos os dados</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button type="submit" class="cadserv btn m-0">Cadastrar serviço</button>
+                        </div>
+                    </div>
+                </footer>
             </div>
         </div>
-
     )
 }
 
