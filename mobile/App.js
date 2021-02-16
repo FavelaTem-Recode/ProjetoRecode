5a2933a08ec02838fc3b9f5a9ceaadbe47c0b545
aripsl <letras.ariana@yahoo.com.br>
2021-02-16 00:02:47 -0300

    Criando projeto em mobile

---
+++ mobile/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StatusBar, Text, View } from 'react-native';
+
+import Login from './src/screens/Login';
+
+
+function App() {
+  return (
+    <View>
+      <StatusBar/>
+
+    <Login />
+      
+    </View>
+  )
+}
+
+export default App;
---
+++ mobile/src/screens/Servico/styles.js
@@ -0,0 +1,125 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    
+    
+    pageservicos: {
+
+        margin: 0,
+        padding: 0,
+    },
+    
+    header: {
+
+        background: white,
+        display: flex,
+        justifycontent: space-between,
+        padding: 5,
+        margin: 0,
+    },
+    
+        navbutton: {
+
+        margin: 5,
+        background: transparent,
+        color: black,
+        outline: 0,
+        border: none,
+    },
+    
+    section2: {
+
+        background: FF4D00,
+        width: "100%",
+        margin: 0,
+        padding: 10,
+                
+    },
+    
+    navbarli: {
+
+        background: transparent,
+    },
+    
+    navbarbutton: {
+
+        margin: 5,
+        background: transparent,
+        outline: 0,
+        border: none,
+        color: white,
+        margin: 0,
+        padding: 0,
+        height: 60,
+    },
+    
+    navbarimg: {
+
+        width: 36,
+        height: 36,
+    },
+    
+    section3: {
+
+        display:flex, 
+    },
+    
+    filtroaside: {
+
+        // backgroundcolor: #1CA8C9,
+        width: 350,
+        height: 300,
+        // border: #1CA8C9,
+        // border-radius: 0px 0px 16px 0px ;
+        color: white,
+        texttransform: uppercase,
+        fontweight: bold,
+        display: flex,
+        justifycontent: center,
+        padding: 0,
+    },
+    
+    ul: {
+
+        padding: 0,
+        margin: 50,
+    },
+    
+    select: {
+
+        height: 40,
+    },
+    
+    cleanFilter: {
+
+        height: 40,
+        background: FF4D00,
+        color: ffffff,
+    },
+    
+    servicos: {
+
+        display: block,
+        textalign: center,
+    },
+    
+    pesquisa: {
+
+        display: flex,
+        alignitems: center,
+        justifycontent: center,
+        margintop: -16,
+    },
+
+    btn: {
+
+        height: 33,
+    },
+    
+   groupservicos: {
+
+        display: flex,
+    },
+    
+    
+export default styles;
