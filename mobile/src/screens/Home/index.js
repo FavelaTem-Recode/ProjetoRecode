a7ba49247a4cef789bea1070d9fbdd61b3a3b150
danielacapassi <danielacapassi_nutricao@hotmail.com>
2021-02-19 19:30:07 -0300

    atualização da página Home

--- mobile/src/screens/Home/index.js
+++ mobile/src/screens/Home/index.js
@@ -1,59 +1,129 @@
-// import React from 'react';
-// import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import React from "react";
+import {
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
 
-// import styles from "./styles";
+import styles from "./styles";
 
-// function Home({ navigation }) {
-//   function servicos() {
-//     navigation.navigate("Servico");
-//   }
-//   return (
-//     <ScrollView style={styles.container}>
-//       <Image
-//         style={styles.imagem}
-//         source={require("../../../assets/logofavelatem.png")}
-//         />
-//       <Text style={styles.titulo}>Seja Bem Vindx!</Text>
-//       <Text style={styles.introducao}>
-//         Página Home: Aqui no FullStack Eletro Mobile Programadores tem desconto!!!
-//       </Text>
-      
-//         <TouchableOpacity style={styles.btn} onPress={servicos}>
-//           <Text style={styles.texto}>Servicos</Text>
-//         </TouchableOpacity>
-//       </ScrollView>
-//     );
-//   }
-  
-//   export default Home;
 
-  import React from 'react';
-  import { View, Text, Image, TouchableOpacity} from 'react-native';
-  
-  
-  import styles from "./styles";
-  
-  function Home({ navigation }) {
-    function servicos() {
-      navigation.navigate("Servico");
-    }
-    return (
-     
-      <View style={styles.container}>
-        <Text style={styles.titulo}>Seja Bem Vindx!</Text>
-        <Text style={styles.introducao}>
-          Página de Home: Traga seu anúncio para cá e comece a conquistar clientes também pela internet
+function Home({ navigation }) {
+  function servico() {
+    navigation.navigate("Servico");
+  }
+  return (
+    <ScrollView style={styles.caixa}>
+      <View style={styles.abertura}>
+        <Text style={styles.titulo}>FAVELA TEM</Text>
+        <Text style={styles.label}>
+          Plataforma de publicidade de serviços dos moradores de Paraisópolis
         </Text>
-        <Image
-          style={styles.imagem}
-          source={require("../../../assets/paraisopolisfoto.png")}
+      </View>
+
+      <View style={styles.inputs}>
+        <TextInput placeholder="O que você procura?" style={styles.input} />
+        <TouchableOpacity style={styles.btn} onPress={servico}>
+          <Text style={styles.texto}> Buscar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.container_normal}>
+        <View style={styles.container}>
+          <Image
+            style={styles.img}
+            source={require("../../../assets/mocaboloroxo.jpg")}
           />
-          <TouchableOpacity style={styles.btn} onPress={servicos}>
-            <Text style={styles.texto}> Cursos</Text>
-          </TouchableOpacity>
+          <Text style={styles.label2}>
+            64 Clientes compartilharam suas experiências aqui
+          </Text>
+          <Text style={styles.label2}>42 profissionais registrados</Text>
+          <Text style={styles.label2}> Dezenas de possibilidades</Text>
         </View>
-      
-      );
-    }
-    
-    export default Home;
+      </View>
+
+      <View style={styles.container_normal}>
+        <View style={styles.container}>
+          <Image
+            style={styles.img}
+            source={require("../../../assets/costura.jpg")}
+          />
+          <Text style={styles.label2}>+ Negócios</Text>
+          <Text style={styles.label2}>
+            Traga seu anúncio para cá e comece a conquistar clientes também pela
+            internet
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.container_row}>
+        <View style={styles.container}>
+          <Text style={styles.label2}>
+            Ao fazer o cadastro você passará por uma trilha de aprendizado que
+            te ensinará a fazer o melhor anúncio do universo!
+          </Text>
+
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <View style={{ margin: 10 }}>
+              <Image
+                style={styles.imgcurso}
+                source={require("../../../assets/empreendedorismo2.png")}
+              />
+            </View>
+
+            <View style={{ margin: 10 }}>
+              <Image
+                style={styles.imgcurso}
+                source={require("../../../assets/financas.png")}
+              />
+            </View>
+
+            <View style={{ margin: 10 }}>
+              <Image
+                style={styles.imgcurso}
+                source={require("../../../assets/mktdigital.png")}
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.container_normal}>
+        <View style={styles.container}>
+          <Text style={styles.label3}>Tudo isso, totalmente Gratuito!!!</Text>
+        </View>
+
+        <TouchableOpacity style={styles.btn} onPress={servico}>
+          <Text style={styles.texto}> Serviços</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.container_normal}>
+        <View style={styles.container}>
+          <Text
+            style={{
+              marginBottom: "auto",
+              textAlign: "center",
+              fontSize: 14,
+              color: "white",
+            }}
+          >
+            © Squad 04 SP 2 -Recode Pro 2020 - 2021
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+export default Home;
+
--- mobile/src/screens/Home/styles.js
+++ mobile/src/screens/Home/styles.js
@@ -1,49 +1,225 @@
 import { StyleSheet, Platform } from "react-native";
 
+// const styles = StyleSheet.create({
+//   container: {
+//     height: "100%",
+//     width: "100%",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#1ca8c9",
+//   },
+//   titulo: {
+//     color: "white",
+//     fontSize: 32,
+//     textAlign: "center",
+//     marginTop: 50,
+//   },
+//   introducao: {
+//     color: "black",
+//     fontSize: 25,
+//     textAlign: "center",
+//     marginTop: 50,
+//   },
+//   imagem: {
+//     width: "95%",
+//     height: "30%",
+//     marginTop: 40,
+//     padding: 20,
+//     borderRadius: 6,
+//     marginTop: 50,
+//     marginLeft: 35,
+//     marginRight: 30,
+//   },
+//   btn: {
+//     backgroundColor: "#ff8C00",
+//     marginTop: 100,
+//     marginBottom: 20,
+//     padding: 10,
+//     borderRadius: 6,
+//     width: 200,
+//     height: 50,
+//   },
+//   texto: {
+//     fontSize: 20,
+//     color: "white",
+//     textAlign: "center",
+//   },
+// });
+
+// export default styles;
+
 const styles = StyleSheet.create({
-  container: {
-    height: "100%",
+  caixa: {
+     height: "100%",
+     width: "100%",
+     backgroundColor: "#1ca8c9"
+
+  },
+    fundo: {
     width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#1ca8c9",
+    height: "100%",
+    // justifyContent: 'center',
+    // alignItems: 'center'
+    
+},
+  label: {
+          color: "black",
+          fontSize: 20,
+          textAlign: "center",
+          marginTop: 5,
+        
   },
-  titulo: {
+  label2: {
     color: "white",
-    fontSize: 32,
+    fontSize: 16,
     textAlign: "center",
-    marginTop: 50,
+    marginTop: 8,
+    marginLeft:18,
+    marginRight:18,
+    padding:0,
+  
+},
+label3: {
+  color: "white",
+  fontSize: 20,
+  textAlign: "center",
+  marginTop: 5,
+
+},
+  inputs: {
+    width: "100%",
+    padding: 8,
   },
-  introducao: {
-    color: "black",
-    fontSize: 25,
-    textAlign: "center",
-    marginTop: 50,
+  input: {
+    backgroundColor: "white",
+    width: "100%",
+    padding: 8,
+    marginTop: 12,
+    borderRadius: 8,
   },
-  imagem: {
-    width: "95%",
-    height: "30%",
-    marginTop: 40,
-    padding: 20,
-    borderRadius: 6,
-    marginTop: 50,
-    marginLeft: 35,
-    marginRight: 30,
+
+  // btnCad: {
+  //   backgroundColor: "#6f42c1",
+  //   marginTop: 12,
+  //   padding: 10,
+  //   borderRadius: 4,
+  //   width: "40%",
+    
+    
+  // },
+  // btnLim: {
+  //   backgroundColor: "#6f42c1",
+  //   marginTop: 12,
+  //   padding: 10,
+  //   borderRadius: 4,
+  //   width: "50%",
+    
+  // },
+  // btns:{
+  //   display: "flex",
+  //   flexDirection: 'row-reverse',
+  //   justifyContent: 'space-between',
+  //   alignItems: "center",
+  // },
+  // texto: {
+  //   fontSize: 18,
+  //   color: "white",
+  //   textAlign: "center",
+  // },
+  // textArea: {
+  //   height: 150,
+  //   backgroundColor: "white",
+  //   width: "100%",
+  //   padding: 8,
+  //   marginTop: 12,
+  //   borderRadius: 8,
+  // },
+img: {
+    width: 230,
+    height: 250,
+    borderRadius: 10,
+    marginLeft: 10
+  },
+  imgcurso: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  // area: {
+  //   display: "flex",
+  //   flexDirection: "row",
+  //   justifyContent: 'space-between',
+  //   alignItems: "center",
+  // }, 
+  abertura: {
+    // display: "flex",
+    // flexDirection: 'column',
+    // justifyContent: 'space-around',
+    alignItems: "center",
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  container_row: {
+    display:"flex",
+    flexDirection: 'row',
+    justifyContent:"space-between",
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#e6aa65"
+  },
+  container_normal: {
+    // flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: "center",
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  container: {
+    marginBottom: 10,
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  
   },
   btn: {
     backgroundColor: "#ff8C00",
-    marginTop: 100,
-    marginBottom: 20,
+    marginTop: 10,
+    marginBottom: 10,
     padding: 10,
     borderRadius: 6,
-    width: 200,
-    height: 50,
+    width: 90,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  
   },
   texto: {
-    fontSize: 20,
+    fontSize: 16,
     color: "white",
     textAlign: "center",
   },
+  titulo: {
+    color: "#ff8C00",
+    fontSize: 40,
+    textAlign: "center",
+    marginTop: 40,
+    marginBottom: 20,
+    fontWeight: "bold",
+  },
+  // introducao: {
+  //   color: "black",
+  //   fontSize: 25,
+  //   textAlign: "center",
+  //   marginTop: 50,
+  //   marginBottom: 30,
+  // },
+  // containercursos: {
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  //   backgroundColor:"green",
+  
+  // }
 });
 
 export default styles;
