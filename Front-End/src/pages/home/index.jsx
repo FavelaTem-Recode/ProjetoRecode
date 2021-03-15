7b02a462991ba47956a511e571782c5851a42cb0
Enzo Silva Soares <enzosoares@protonmail.com>
2021-03-15 00:23:26 -0300

    Terminando responsividade pagina inicial

--- Front-End/src/pages/home/index.jsx
+++ Front-End/src/pages/home/index.jsx
@@ -7,8 +7,8 @@ import logo from "../../assets/imagens/logofavelatem.png";
 import financas from "../../assets/imagens/financas.png";
 import marketing from "../../assets/imagens/mktdigital.png";
 import empreendedorismo from "../../assets/imagens/empreendedorismo2.png";
-// import "./style.css";
-import './stylo.css';
+
+import './style.css';
 
 import Menu from "../../componentes/Menu";
 
@@ -33,57 +33,56 @@ const Home = () => {
     }, [])
 
     return (
-        <main className="principal">
+        <main className="home">
             <Menu />
             <section className="container-fluid col p-0 secao1" >
-                <div>
+                <div className="w-75">
                     <h1>o que você procura?</h1>
-                    <div className="col-lg-6 pesquisar">
-                        <input className="pesquisa" type='text' placeholder='Ex. Informática' name='buscar' value={pesquisa} onChange={onChangeHandler} />
+                    <div className="col-lg-6 pesquisar form-group">
+                        <input className="pesquisa form-control" type='text' placeholder='Ex. Informática' name='buscar' value={pesquisa} onChange={onChangeHandler} />
                         <Link className='btn-buscar' type='button' to={{
-                            pathname: "serviços",
-                            search: "?&pesquisar=" + pesquisa
-                            }}>Buscar
+                            pathname: "servicos",
+                            search: "?&pesquisa=" + pesquisa
+                        }}>Buscar
                         </Link>
                     </div>
-                </div>             
+                </div>
             </section>
 
-            <section className="container-fluid col-lg-6 secao2">
-                
-                <div className=' img-sec2'>
-                    <img src={mocabrx} alt="mulher com bolo" />
+            <section className="container-fluid row secao2 mx-0 my-2">
+
+                <div className='img-sec2 col-lg-6'>
+                    <img src={mocabrx} alt="mulher com bolo" className="img-fluid" />
                 </div>
 
-                <div className=' col-lg-6 dados-sec2' >
-                    <div className='row  '>
-                        <h1>{dados[0].qntdComentarios}</h1>
-                        <p>clientes compartilharam<br /> suas experiências aqui</p>
+                <div className='col-lg-6 dados-sec2'>
+                    <div className='w-75 d-inline-block my-2'>
+                        <h1 className="d-inline">{dados[0].qntdComentarios}</h1>
+                        <p className="d-inline">clientes compartilharam suas experiências aqui</p>
                     </div>
-                    <div className='row'>
-                        <h1>{dados[0].qntdPrestadores}</h1>
-                        <p>profissionais registrados</p>
+                    <div className='w-75 d-inline-block my-2'>
+                        <h1 className="d-inline">{dados[0].qntdPrestadores}</h1>
+                        <p className="d-inline">profissionais registrados</p>
                     </div>
-                    <div className='row'>
-                        <p>dezenas <br />
-                        de possibilidades</p>
+                    <div className='text-center'>
+                        <h1>DEZENAS DE POSSIBILIDADES</h1>
                     </div>
                 </div>
-                
+
             </section>
 
             <section className='container-fluid row secao3'>
-                
-                <div className='col-lg-6 sec3-cont1'>
+
+                <div className='col-lg-4 sec3-cont1 d-flex alignt-items-end justify-content-center'>
                     <h1>+ Negócios</h1>
-                    <p>traga seu anúncio e comece <br/>a conquistar clientes também pela internet</p>
-                    <input type="button" value="ANÚNCIAR" />
+                    <p>traga seu anúncio e comece a conquistar clientes também pela internet</p>
+                    <Link to="/login"><input type="button" value="ANUNCIAR" /></Link>
                 </div>
 
                 <div className='img-sec3 '>
                     <img class="img-fluid" src={costura} alt="Oficina de costura" />
-                </div>  
-                
+                </div>
+
             </section>
 
             <section className='container-fluid secao4'>
@@ -91,11 +90,11 @@ const Home = () => {
             </section>
 
             <section className="container-fluid secao5">
-                
+
                 <div className="row img-sec5 ">
-                    <img style={{margin: 10}} className='img-fluid cursos' src={financas} alt="Curso de finanças" />
-                    <img style={{margin: 10}} className='img-fluid cursos' src={marketing} alt="Curso de marketing digital" />
-                    <img style={{margin: 10}} className='img-fluid cursos' src={empreendedorismo} alt="Curso empreendedorismo" />
+                    <img style={{ margin: 10 }} className='img-fluid cursos' src={financas} alt="Curso de finanças" />
+                    <img style={{ margin: 10 }} className='img-fluid cursos' src={marketing} alt="Curso de marketing digital" />
+                    <img style={{ margin: 10 }} className='img-fluid cursos' src={empreendedorismo} alt="Curso empreendedorismo" />
                 </div>
 
                 <div className='text-sec5'>
@@ -104,36 +103,36 @@ const Home = () => {
                     usar a plataforma, mas também como fazer o
                     melhor anúncio!</p>
                 </div>
-               
+
             </section>
-            
-            <section className="container-fluid secao6">
-                
+
+            <section className="container-fluid secao6 text-center">
+
                 <p className='text1-sec6'>tudo isso, totalmente gratuito!!</p>
 
             </section>
 
-            <section className='container-fluid  bg-btn-sec7'>    
-                
+            <section className='container-fluid  bg-btn-sec7'>
+
                 <div className='btn-registro'>
                     <Link class="button" to="/registro" type="button">Cadastrar-se agora</Link>
                 </div>
-                
+
             </section>
 
-            <section className="container-fluid row secao7 ">    
+            <section className="container-fluid row secao7 ">
                 <div>
                     <img calssName="img-fluid" src={logo} alt="logotipo" />
                 </div>
-                
-                <div ClassName="container-fluid text-sec7">
+
+                <div ClassName="container-fluid text-sec7 text-center">
                     <p> O Favela Tem foi desenvolvido por um grupo de jovens,
-                    com o propósito de divulgar os serviços de moradores de paraisópolis 
+                    com o propósito de divulgar os serviços de moradores de paraisópolis
                     que não possuem ponto comercial, para que aumentem suas carteiras de clientes através da internet.
                     Além de empoderar esses trabalhadores com um conteúdo educacional em forma de gamificação.
                     </p>
-                </div>                
-                
+                </div>
+
 
                 <div className="contato">
                     <p >Contato</p>
